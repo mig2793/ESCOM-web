959ddcacab19a1e2c9473d053610bb291c7d92e4
mig2793 <miguel.angel.gonzalez.pinto@hotmail.com>
2016-10-09 13:01:15 -0500

    Inicio Proyecto WEB ESCOM. Solicitud de Equipos de laborario

---
+++ app/app.module.js
@@ -0,0 +1,7 @@
+ var up2u = angular.module('up2u', ['ui.router', 'angularModalService']); //,'mobile-angular-ui'
+
+setTimeout(function(){
+	try{
+		navigator.splashscreen.hide();
+	}catch(e){}
+}, 3000);
